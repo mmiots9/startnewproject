739210ebc546e34eb92721a64dd8735dd4a6223f
Matteo Miotto pro <matteo.miotto9@gmail.com>
2024-11-28 16:59:39 +0100

    Version 0.1.8
    Docs for 0.1.8 version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import startnewproject
 project = 'startnewproject'
 copyright = '2024, Matteo Miotto'
 author = 'Matteo Miotto'
-release = '0.1.7'
+release = '0.1.8'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
